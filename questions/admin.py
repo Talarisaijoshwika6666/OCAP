5c6f47aafdde7fe170ba680af80a976d111482ac
Talarisaijoshwika6666 <saijoshwikatalari@gmail.com>
2026-07-06 11:48:06 +0530

    Resolved merge

--- questions/admin.py
+++ questions/admin.py
@@ -8,15 +8,12 @@ class TestCaseInline(admin.TabularInline):
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ['title', 'difficulty']
-<<<<<<< HEAD
     fields = ['title', 'description', 'difficulty', 'sample_input', 'sample_output', 'hint', 'answer', 'time_limit']
     inlines = [TestCaseInline]
     
-=======
     inlines = [TestCaseInline]
 
 @admin.register(Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
     list_display = ['user', 'question', 'created_at']
     list_filter = ['created_at']
->>>>>>> origin/ganesh
